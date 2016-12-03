6be41e19ddb5e5c02ecc0388ed74bcc398562624
Rohit <rohitvvv@gmail.com>
2016-12-03 23:57:12 +0530

    Adding basic Loops

---
+++ PlayWithLoops.py
@@ -0,0 +1,11 @@
+for food in "fruits", "vegitables", "drinks":
+    print "I love ", food
+
+for number in range(1,100):
+    print "hello world"
+
+x = 10
+while x > 0:
+    print x
+    x=x-1
+print "While loop Completes"
